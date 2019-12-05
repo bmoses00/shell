6c1575775bed4759ca4adae49dc7edf969474854
bmoses00 <bmoses00@stuy.edu>
2019-12-05 14:50:43 -0500

    Does not work

--- main.c
+++ main.c
@@ -9,20 +9,15 @@
 # include <time.h>
 # include <sys/wait.h>
 
-char ** parse_args(char * line) {
+char ** parse_args(char * line, char * delimiter) {
 	char ** args = calloc(48, 1);
 	int i;
 	for (i = 0; line != NULL; i++) {
 		args[i] = calloc(8, 1);
-		args[i] = strsep(&line, " ");
+		args[i] = strsep(&line, delimiter);
 	}
 	args[i] = NULL;
 	return args;
-}
-
-char *** parse_comms(char * line){
-	char *** comms = calloc(100, 1);
-	int i;
 }
 
 int main(int argc, char * argv[]) {
@@ -30,22 +25,27 @@ int main(int argc, char * argv[]) {
 	char ** args;
 	int f, w;
 	printf("%s# ", getcwd(s, 100));
-	
+
 	while (1) {
 		// take user input
 		fgets(buffer, 100, stdin);
 		buffer[strlen(buffer)-1] = 0;
-		args = parse_args(buffer);
-		// special handling for cd and exit
-		if (strcmp(args[0], "exit") == 0) return 0;
-		if (strcmp(args[0],  "cd" ) == 0) chdir(args[1]);
-		else {
-			// forking and executing commands
-			f = fork();
-			if (f == 0)
-				execvp(args[0], args);
-	  }
-		wait(&w);
+		args = parse_args(buffer, ";");
+		// args is now an array of Strings. Each String is a command, ex ls -l.
+		int i;
+		for (i = 0 ; args[i] != NULL; i++) {
+			something = parse_args(something, " ");
+			// special handling for cd and exit
+			if (strcmp(args[0], "exit") == 0) return 0;
+			if (strcmp(args[0],  "cd" ) == 0) chdir(args[1]);
+			else {
+				// forking and executing commands
+				f = fork();
+				if (f == 0)
+					execvp(args[0], args);
+		  }
+			wait(&w);
+		}
 		printf("%s# ", getcwd(s, 100));
 	}
 
