e39eefc1d65e547894f603d03bde2fcca881e4a9
Brian <bmoses00@stuy.edu>
2019-12-07 18:13:02 -0500

    "Added

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <string.h>
-# include <sys/stat.h>
 # include <dirent.h>
-# include <sys/types.h>
 # include <errno.h>
 # include <unistd.h>
-# include <time.h>
+# include <fcntl.h>
 # include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
 
 char ** parse_args(char * line, char * delimiter) {
 	char * buff = calloc(100, 1);
@@ -30,7 +30,6 @@ char * remove_spaces(char * input) {
 	if (input[strlen(input) - 1] == 32) {
 		input[strlen(input) - 1] = 0;
 	}
-
 	return input;
 }
 
@@ -38,37 +37,63 @@ int main(int argc, char * argv[]) {
 	char buffer[100], s[100];
 	char ** args;
 	char ** commands;
-	int f, w, e;
+	char ** test;
+	int fork_status, wait_status, error, temp_stdout_fileno, fd;
 	printf("%s# ", getcwd(s, 100));
 
 	while (1) {
 		// take user input
 		fgets(buffer, 100, stdin);
 		buffer[strlen(buffer) - 1] = 0;
+		// commands is now an array of Strings. Each String is a command, ex ls -l.
 		commands = parse_args(buffer, ";");
-		// args is now an array of Strings. Each String is a command, ex ls -l.
 		int i;
 		for (i = 0 ; commands[i] != NULL; i++) {
 			commands[i] = remove_spaces(commands[i]);
 			args = parse_args(commands[i], " ");
-			// special handling for cd and exit
-			if (strcmp(args[0], "exit") == 0) {
-				return 0;
-			}
-			if (strcmp(args[0],  "cd" ) == 0) {
-				chdir(args[1]);
+
+			// 62 is ASCII char for '>'
+			if (strchr(commands[i], 62) != NULL) {
+				test = parse_args(commands[i], ">");
+				args = parse_args(remove_spaces(test[0]), " ");
+				char * file_name = remove_spaces(test[1]);
+
+				fd = open(file_name, O_CREAT|O_EXCL|O_WRONLY, 0755);
+				if (fd == -1) {
+					fd = open(file_name, O_WRONLY);
+				}
+
+				temp_stdout_fileno = dup(1);
+				dup2(fd, 1);
+
+				fork_status = fork();
+				if (fork_status == 0) {
+					error = execvp(args[0], args);
+					if (error == -1) return 0;
+				}
+
+				dup2(temp_stdout_fileno, 1);
+
 			}
 			else {
-				// forking and executing commands
-				f = fork();
-				if (f == 0) {
-					e = execvp(args[0], args);
-					if (e == -1) return 0;
+				// special handling for cd and exit
+				if (strcmp(args[0], "exit") == 0) {
+					return 0;
 				}
-		  }
-			wait(&w);
+				if (strcmp(args[0],  "cd" ) == 0) {
+					chdir(args[1]);
+				}
+				else {
+					// forking and executing commands
+					fork_status = fork();
+					if (fork_status == 0) {
+						error = execvp(args[0], args);
+						if (error == -1) return 0;
+					}
+			  }
+			}
+			wait(&wait_status);
 		}
 		printf("%s# ", getcwd(s, 100));
 	}
-
 }
